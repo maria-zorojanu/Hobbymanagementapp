df19cf107d242c20f9adb9aaaaaba2bf255d3eee
Maria Zorojanu <mariazorojanu126@gmail.com>
2023-04-30 13:07:11 +0300

    functionalitate de log in si de register

--- app/src/main/java/com/example/hobbymanagementapp/LogInActivity.java
+++ app/src/main/java/com/example/hobbymanagementapp/LogInActivity.java
@@ -1,20 +1,103 @@
 package com.example.hobbymanagementapp;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 
 public class LogInActivity extends AppCompatActivity {
+
+    EditText emailEditText;
+    EditText passwordEditText;
+    private FirebaseAuth mAuth;
+    SharedPreferences sharedPreferences;
+    String displayName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        emailEditText = findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+        mAuth = FirebaseAuth.getInstance();
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("com.example.hobbymanagementapp", Context.MODE_PRIVATE);
+        String email = sharedPreferences.getString("email", "");
+        String password = sharedPreferences.getString("password", "");
+        //String username = sharedPreferences.getString("userName", "");
+        if(email.length()>0 && password.length()>0)
+        {
+            Log.i("shared preferences:", email +' '+ password);
+            firebaseLogIn(email, password);
+        }
     }
 
     public void register(View view) {
+        Log.i("register pressed", "ok");
+
+        Intent intentRegister = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(intentRegister);
     }
 
     public void login(View view) {
+        Log.i("login pressed", "ok");
+
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        Log.i("email & pwd", email + " "+ password);
+        if(email.length()>0 && password.length()>0) {
+            firebaseLogIn(email, password);
+        }
+    }
+    public void firebaseLogIn(String email,String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.d("task", "success");
+                            sharedPreferences.edit().putString("email", email).apply();
+                            sharedPreferences.edit().putString("password", password).apply();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            displayName = user.getDisplayName();
+
+                            for (UserInfo userInfo : user.getProviderData()) {
+                                if (displayName == null && userInfo.getDisplayName() != null) {
+                                    displayName = userInfo.getDisplayName();
+                                }
+                            }
+
+                            String id = "";
+                            if(displayName!= null)
+                            {
+                                Log.i("name", displayName);
+                            }
+                            id = user.getUid();
+                            Log.i("id", id);
+                            //Intent mainMenuIntent = new Intent(getApplicationContext(), MainMenu.class);
+                            sharedPreferences.edit().putString("userName", displayName).apply();
+                            sharedPreferences.edit().putString("uid", id).apply();
+                            //startActivity(mainMenuIntent);
+                        }else{
+                            Log.d("log in", "fail");
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/hobbymanagementapp/RegisterActivity.java
+++ app/src/main/java/com/example/hobbymanagementapp/RegisterActivity.java
@@ -1,13 +1,90 @@
 package com.example.hobbymanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText emailEditText;
+    EditText passwordEditText;
+    EditText nameEditText;
+    private FirebaseAuth mAuth;
+    SharedPreferences sharedPreferences;
+    String id = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        emailEditText = findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+        nameEditText = findViewById(R.id.editTextName);
+
+        mAuth = FirebaseAuth.getInstance();
+        sharedPreferences = getApplicationContext().getSharedPreferences("com.example.hobbymanagementapp", Context.MODE_PRIVATE);
+
+
+    }
+
+    public void register(View view) {
+        Log.i("register pressed", "ok");
+
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String name = nameEditText.getText().toString();
+
+        Log.i("email & pwd", email + " "+ password);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Log.i("task","successful");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(name).build();
+                            user.updateProfile(profileUpdates);
+                            id = user.getUid();
+                            sharedPreferences.edit().putString("email", email).apply();
+                            sharedPreferences.edit().putString("password", password).apply();
+                            sharedPreferences.edit().putString("userName", name).apply();
+                            sharedPreferences.edit().putString("uid", id).apply();
+
+                            //Intent mainMenuIntent = new Intent(getApplicationContext(), MainMenu.class);
+                            //startActivity(mainMenuIntent);
+                        }else{
+                            Log.i("task","failure");
+                            Toast toast = Toast.makeText(getApplicationContext(), "Could not register", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    }
+                });
     }
 }
